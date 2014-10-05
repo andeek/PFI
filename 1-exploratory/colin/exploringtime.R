5eb9677b737cdf649410e08702270a22e1eecac1
clewisbe <clewisbe@iastate.edu>
2014-10-05 17:56:18 -0500

    More Time Series Looks
    Looked at Time Series for Crop Income and Yield using ggplot2.  No
    obvious trends so far.

--- 1-exploratory/colin/exploringtime.R
+++ 1-exploratory/colin/exploringtime.R
@@ -42,12 +42,59 @@ t5_cropinc <- cbind(pfi$year[which(pfi$field_id=="Thompson5" & pfi$item=="Crop_I
                                                                                                             pfi$item=="Crop_Income")])
 colnames(t5_cropinc) <- c("year","cropinc")
 
-#time series with all plots
+#time series of crop income, red is neighbor, black is Thompson
+#It doesn't look like he's doing much better than his neighbor
 
-plot(b1_cropinc,col="black")
-points(b2_cropinc,col="black")
-points(t1_cropinc,col="red")
-points(t2_cropinc,col="red")
-points(t3_cropinc,col="red")
-points(t4_cropinc,col="red")
-points(t5_cropinc,col="red")
+ggplot(data.frame(b1_cropinc),aes(year,cropinc),col="red")+geom_point(col="red")+
+geom_line(col="red")+geom_point(data=data.frame(b2_cropinc),aes(year,cropinc),col="red")+
+  geom_line(data=data.frame(b2_cropinc),aes(year,cropinc),col="red")+geom_point(data=data.frame(t1_cropinc),aes(year,cropinc),col="black")+
+  geom_line(data=data.frame(t1_cropinc),aes(year,cropinc),col="black")+geom_point(data=data.frame(t2_cropinc),aes(year,cropinc),col="black")+
+  geom_line(data=data.frame(t2_cropinc),aes(year,cropinc),col="black")+geom_point(data=data.frame(t3_cropinc),aes(year,cropinc),col="black")+
+  geom_line(data=data.frame(t3_cropinc),aes(year,cropinc),col="black")+geom_point(data=data.frame(t4_cropinc),aes(year,cropinc),col="black")+
+  geom_line(data=data.frame(t4_cropinc),aes(year,cropinc),col="black")+geom_point(data=data.frame(t5_cropinc),aes(year,cropinc),col="black")+
+  geom_line(data=data.frame(t5_cropinc),aes(year,cropinc),col="black")
+
+#Time Series for Crop Yield Per Acre
+
+b1_yield <- cbind(pfi$year[which(pfi$field_id=="Boone1" & pfi$item=="Yield_Per_Acre_Bu_per_pound")],pfi$value2[which(pfi$field_id=="Boone1" & 
+                                                                                                         pfi$item=="Yield_Per_Acre_Bu_per_pound")])
+colnames(b1_yield) <- c("year","yield")
+
+b2_yield <- cbind(pfi$year[which(pfi$field_id=="Boone2" & pfi$item=="Yield_Per_Acre_Bu_per_pound")],pfi$value2[which(pfi$field_id=="Boone2" & 
+                                                                                                                       pfi$item=="Yield_Per_Acre_Bu_per_pound")])
+colnames(b2_yield) <- c("year","yield")
+
+t1_yield <- cbind(pfi$year[which(pfi$field_id=="Thompson1" & pfi$item=="Yield_Per_Acre_Bu_per_pound")],pfi$value2[which(pfi$field_id=="Thompson1" & 
+                                                                                                                       pfi$item=="Yield_Per_Acre_Bu_per_pound")])
+colnames(t1_yield) <- c("year","yield")
+
+t2_yield <- cbind(pfi$year[which(pfi$field_id=="Thompson2" & pfi$item=="Yield_Per_Acre_Bu_per_pound")],pfi$value2[which(pfi$field_id=="Thompson2" & 
+                                                                                                                          pfi$item=="Yield_Per_Acre_Bu_per_pound")])
+colnames(t2_yield) <- c("year","yield")
+
+t3_yield <- cbind(pfi$year[which(pfi$field_id=="Thompson3" & pfi$item=="Yield_Per_Acre_Bu_per_pound")],pfi$value2[which(pfi$field_id=="Thompson3" & 
+                                                                                                                          pfi$item=="Yield_Per_Acre_Bu_per_pound")])
+colnames(t3_yield) <- c("year","yield")
+
+t4_yield <- cbind(pfi$year[which(pfi$field_id=="Thompson4" & pfi$item=="Yield_Per_Acre_Bu_per_pound")],pfi$value2[which(pfi$field_id=="Thompson4" & 
+                                                                                                                          pfi$item=="Yield_Per_Acre_Bu_per_pound")])
+colnames(t4_yield) <- c("year","yield")
+
+t5_yield <- cbind(pfi$year[which(pfi$field_id=="Thompson5" & pfi$item=="Yield_Per_Acre_Bu_per_pound")],pfi$value2[which(pfi$field_id=="Thompson5" & 
+                                                                                                                          pfi$item=="Yield_Per_Acre_Bu_per_pound")])
+colnames(t5_yield) <- c("year","yield")
+
+
+ggplot(data.frame(b1_yield),aes(year,yield),col="red")+geom_point(col="red")+
+  geom_line(col="red")+geom_point(data=data.frame(b2_yield),aes(year,yield),col="red")+
+  geom_line(data=data.frame(b2_yield),aes(year,yield),col="red")+geom_point(data=data.frame(t1_yield),aes(year,yield),col="black")+
+  geom_line(data=data.frame(t1_yield),aes(year,yield),col="black")+geom_point(data=data.frame(t2_yield),aes(year,yield),col="black")+
+  geom_line(data=data.frame(t2_yield),aes(year,yield),col="black")+geom_point(data=data.frame(t3_yield),aes(year,yield),col="black")+
+  geom_line(data=data.frame(t3_yield),aes(year,yield),col="black")+geom_point(data=data.frame(t4_yield),aes(year,yield),col="black")+
+  geom_line(data=data.frame(t4_yield),aes(year,yield),col="black")
+
+#Thompson5 was left off as it's values are on another scale and mess up the graph.
+#No obvious patterns seem to exist when comparing yields for Thompson vs. Boone.
+
+
+
