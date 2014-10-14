d80045e19db09a851b6b301ed7e2dce175299428
Andee Kaplan <ajkaplan@iastate.edu>
2014-10-14 14:58:44 -0500

    added some more views, time based

--- 1-exploratory/andee/eda.R
+++ 1-exploratory/andee/eda.R
@@ -14,3 +14,41 @@ dat_2000.sum <- ddply(dat_2000, .(field_id, crop, item_type), summarise, avg=mea
 qplot(item_type, total, data=dat_2000.sum[-c(grep("Unit", dat_2000.sum$item_type), grep("Derived", dat_2000.sum$item_type)),], colour=substring(field_id, 1, nchar(as.character(field_id)) - 1)) + 
   facet_wrap(~crop) +
   scale_colour_discrete("Crop System")
+
+rev_exp_compare <- ddply(subset(dat, item_type %in% c("Revenue", "Expense")), 
+      .(year, substring(field_id, 1, nchar(as.character(field_id)) - 1), item_type),
+      summarise,
+      avg = mean(value))
+names(rev_exp_compare)[2] <- "farmer"
+
+ggplot(data=rev_exp_compare) +
+  geom_line(aes(year, avg, group=farmer, colour=farmer)) + 
+  facet_wrap(~item_type, scales="free_y")
+
+return_compare <- ddply(subset(dat, item %in% c("LaborandMR_dollar_per_A", "Labor_Return")),
+                        .(year, substring(field_id, 1, nchar(as.character(field_id)) - 1), item),
+                        summarise,
+                        avg=mean(value))
+names(return_compare)[2] <- "farmer"
+                        
+ggplot(data=return_compare) +
+  geom_line(aes(year, avg, group=farmer, colour=farmer)) +
+  facet_wrap(~item, scales="free_y")
+
+
+## Moving average -- 5 year
+library(dplyr)
+ma <- function(x,n=5){stats::filter(x,rep(1/n,n), sides=2)}
+
+return_compare_filter <- return_compare %>% group_by(farmer, item) %>%
+  mutate(ma_avg = ma(avg))
+
+ggplot(data=return_compare_filter) +
+  geom_line(aes(year, ma_avg, group=farmer, colour=farmer)) +
+  geom_line(aes(year, avg, group=farmer, colour=farmer), lty=2) +
+  facet_wrap(~item, scales="free_y")
+
+## It seems like there is a stark difference in the rate of return post 2000. 
+## Explore what that difference might be
+dat$post_2000 <- year >= 2000
+
